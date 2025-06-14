b49a6ff4378642ae826f952fd9d2d0b6a4dce0be
dzhukow <128096484+dzhukow@users.noreply.github.com>
2025-06-14 21:39:06 +0300

    Update customizer.sh

--- devices/gk7205v300_lite_camhi-RTL8188/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7205v300_lite_camhi-RTL8188/general/overlay/usr/share/openipc/customizer.sh
@@ -5,7 +5,7 @@
 #
 # Set custom upgrade url
 #
-fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v300_lite_vixand-ivg-g6s-w-nor.tgz'
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v300_lite_camhi-RTL8188.tgz'
 #
 #
 # Set wlan device and credentials if need
